b4241d1f551b4b5f089ca1ccee2bd8e5fd245565
Jacob Getto <jgetto@nuodb.com>
2013-05-28 11:57:31 -0400

    added numeric test

--- tests/nuodb_basic_test.py
+++ tests/nuodb_basic_test.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+"""
+These tests assume that the quickstart database exists.
+
+To create it run /opt/nuodb/run-quickstart or use the web console.
+"""
+
 import pynuodb
 import unittest
 
@@ -23,6 +29,38 @@ class NuoDBBasicTest(NuoBase):
         row = cursor.fetchone()
         self.assertEqual(len(row), 1)
         self.assertEqual(row[0], 1)
+        
+    def test_numeric_types(self):
+        con = self._connect()
+        cursor = con.cursor()
+        try:
+            cursor.execute("create table typetest (id integer GENERATED ALWAYS AS IDENTITY, smallint_col smallint, integer_col integer, bigint_col bigint, " + 
+                           "numeric_col numeric(10, 2), decimal_col decimal(10, 2), number_col number, double_col double)")
+            
+            # Basic test
+            cursor.execute("insert into typetest (smallint_col, integer_col, bigint_col, numeric_col, decimal_col, number_col, double_col) " +
+                           "values (0, 0, 0, 0, 0, 0, 0)")
+            
+            cursor.execute("select * from typetest order by id desc limit 1")
+            row = cursor.fetchone()
+            
+            for num in row:
+                self.assertEqual(num, 0);
+                
+                
+            # param test
+            test_vals = (-3424, 23453464, 45453453454545, 234355.33, 976.2, 34524584057.3434234, 10000.999)
+            cursor.execute("insert into typetest (smallint_col, integer_col, bigint_col, numeric_col, decimal_col, number_col, double_col) " +
+                           "values (?, ?, ?, ?, ?, ?, ?)", test_vals)
+            
+            cursor.execute("select * from typetest order by id desc limit 1")
+            row = cursor.fetchone()
+            
+            for i in xrange(0, len(row)):
+                self.assertEqual(row[i], test_vals[i]);
+            
+        finally:
+            cursor.execute("drop table typetest if exists")
         
 if __name__ == '__main__':
     unittest.main()
