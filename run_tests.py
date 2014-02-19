735021b2ce68a1eb15d7537af4c02a42148ef48e
Jacob Getto <jgetto@nuodb.com>
2014-02-19 18:53:05 -0500

    get tests running with builtin discovery

--- run_tests.py
+++ run_tests.py
@@ -1,17 +1,6 @@
-from tests.nuodb_basic_test import *
-from tests.nuodb_executionflow_test import *
-from tests.nuodb_dbapi20_test import *
-from tests.nuodb_transaction_test import *
-from tests.nuodb_globals_test import *
-from tests.nuodb_cursor_test import *
-from tests.nuodb_blob_test import *
-from tests.nuodb_huge_test import *
-from tests.nuodb_connect_test import *
-
 import unittest
-import os
 
-path = os.getcwd()
-os.chdir(path + '/tests')
+suite = unittest.TestLoader().discover('tests', pattern="*_test.py")
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.TextTestRunner().run(suite)
