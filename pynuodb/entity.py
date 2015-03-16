06b5c3fa05ac849c050ef7d25e3dda78e17c1878
Alex Charis <acharis@nuodb.com>
2015-03-16 16:21:52 -0400

    no longer mutate options in start_storage_manager, close issue #36

--- pynuodb/entity.py
+++ pynuodb/entity.py
@@ -606,12 +606,7 @@ class Peer:
         if not options:
             options = []
 
-        options.append(("--archive", archive))
-
-        if initialize:
-            options.append(("--initialize", None))
-
-        return self.__start_process(db_name, options, wait_seconds)
+        return self.__start_process(db_name, options + [("--archive", archive)] + ([("--initialize", None)] if initialize else []), wait_seconds)
 
     def __start_process(self, db_name, options, wait_seconds):
         """
