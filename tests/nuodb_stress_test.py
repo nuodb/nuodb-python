3b193ffa71c26a6540bb79fd2dfa3a1cf08a237d
Tim Vincent <tvincent@nuodb.com>
2015-05-20 15:30:08 -0400

    Added 1000 connection and 1M row tests

---
+++ tests/nuodb_stress_test.py
@@ -0,0 +1,42 @@
+import pynuodb.util
+import unittest
+import sys
+
+from nuodb_base import NuoBase
+
+HOST            = "localhost"
+DOMAIN_USER     = "domain"
+DOMAIN_PASSWORD = "bird"
+
+DBA_USER        = 'dba'
+DBA_PASSWORD    = 'dba_password'
+DATABASE_NAME   = 'pynuodb_test'
+
+class NuoDBStressTest(NuoBase):
+
+	def test_million_reads(self):
+		connection = pynuodb.connect(DATABASE_NAME, HOST, DBA_USER, DBA_PASSWORD, options={'schema':'hockey'})
+		cursor = connection.cursor()
+		alphabet = "abcdefghijklmnopqrstuwxyz"
+
+		cursor.execute("USE test")
+		cursor.execute("DROP TABLE hundred IF EXISTS")
+		cursor.execute("CREATE TABLE hundred (f1 INTEGER)")
+
+		for i in range(100):
+			cursor.execute("INSERT INTO hundred VALUES (?)", [i])
+		connection.commit();
+
+		cursor.execute("SELECT ? FROM hundred AS a1, hundred AS a2, hundred AS a3", [ alphabet ])
+
+		#Fetch the next 1,000,000 results 
+		for i in xrange(1000000):
+			rowArray = cursor.fetchone()
+
+		cursor.close()
+		connection.close()
+
+	def test_hundred_connection(self):
+		for i in range(0, 1000):
+			connection = pynuodb.connect(DATABASE_NAME, HOST, DBA_USER, DBA_PASSWORD, options={'schema':'hockey'})
+			connection.close()
