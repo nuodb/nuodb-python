e3420908c4cd10466f67e2118c9b7b9f788e63ce
Tim Vincent <tvincent@nuodb.com>
2015-06-04 10:51:08 -0400

    Updated crypt test values for python 2 and 3

--- tests/nuodb_crypt_test.py
+++ tests/nuodb_crypt_test.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python
 
 import unittest
-
+import binascii
 import pynuodb
 
 
 class NuoDBBasicTest(unittest.TestCase):
     def test_toByteString(self):
-        self.assertEqual(pynuodb.crypt.toSignedByteString(1), '01'.decode('hex'))
-        self.assertEqual(pynuodb.crypt.toSignedByteString(127), '7F'.decode('hex'))
-        self.assertEqual(pynuodb.crypt.toSignedByteString(254), '00FE'.decode('hex'))
-        self.assertEqual(pynuodb.crypt.toSignedByteString(255), '00FF'.decode('hex'))
-        self.assertEqual(pynuodb.crypt.toSignedByteString(-1), 'FF'.decode('hex'))
-        self.assertEqual(pynuodb.crypt.toSignedByteString(-2), 'FE'.decode('hex'))
-        self.assertEqual(pynuodb.crypt.toSignedByteString(-256), 'FF00'.decode('hex'))
-        self.assertEqual(pynuodb.crypt.toSignedByteString(-258), 'FEFE'.decode('hex'))
+        self.assertEqual(pynuodb.crypt.toSignedByteString(1), '\x01')
+        self.assertEqual(pynuodb.crypt.toSignedByteString(127), '\x7f')
+        self.assertEqual(pynuodb.crypt.toSignedByteString(254), '\x00\xfe')
+        self.assertEqual(pynuodb.crypt.toSignedByteString(255), '\x00\xff')
+        self.assertEqual(pynuodb.crypt.toSignedByteString(-1), '\xff')
+        self.assertEqual(pynuodb.crypt.toSignedByteString(-2), '\xfe')
+        self.assertEqual(pynuodb.crypt.toSignedByteString(-256), '\xff\x00')
+        self.assertEqual(pynuodb.crypt.toSignedByteString(-258), '\xfe\xfe')
 
     def test_fromByteString(self):
-        self.assertEqual(pynuodb.crypt.fromSignedByteString('01'.decode('hex')), 1)
-        self.assertEqual(pynuodb.crypt.fromSignedByteString('00FF'.decode('hex')), 255)
-        self.assertEqual(pynuodb.crypt.fromSignedByteString('FF'.decode('hex')), -1)
-        self.assertEqual(pynuodb.crypt.fromSignedByteString('FF01'.decode('hex')), -255)
-        self.assertEqual(pynuodb.crypt.fromSignedByteString('FF00'.decode('hex')), -256)
-        self.assertEqual(pynuodb.crypt.fromSignedByteString('FEFE'.decode('hex')), -258)
+        self.assertEqual(pynuodb.crypt.fromSignedByteString('\x01'), 1)
+        self.assertEqual(pynuodb.crypt.fromSignedByteString('\x00\xff'), 255)
+        self.assertEqual(pynuodb.crypt.fromSignedByteString('\xff'), -1)
+        self.assertEqual(pynuodb.crypt.fromSignedByteString('\xff\x01'), -255)
+        self.assertEqual(pynuodb.crypt.fromSignedByteString('\xff\x00'), -256)
+        self.assertEqual(pynuodb.crypt.fromSignedByteString('\xfe\xfe'), -258)
 
     def test_bothByteString(self):
         self.assertEqual(pynuodb.crypt.fromSignedByteString(pynuodb.crypt.toSignedByteString(1)), 1)
