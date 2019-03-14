8365b5437870b50a372cf341562fd1c86af61703
David Bain <dbain@nuodb.com>
2019-03-14 15:23:19 -0400

    Only import on windows

--- pynuodb/crypt.py
+++ pynuodb/crypt.py
@@ -22,11 +22,14 @@ import hashlib
 import random
 import binascii
 import sys
-try:
-    from cryptography.hazmat.backends import default_backend
-    from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-    cryptographyImported = True
-except ImportError:
+if sys.platform == 'linux' or sys.platform == 'linux2':
+    try:
+        from cryptography.hazmat.backends import default_backend
+        from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+        cryptographyImported = True
+    except ImportError:
+        cryptographyImported = False
+else:
     cryptographyImported = False
 
 systemVersion = sys.version[0]
