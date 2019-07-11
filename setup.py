a67bf3fcadc3ebc0eba66bef5f41aabd72fb6d32
Paul Smith <paul@mad-scientist.net>
2019-07-11 17:10:01 -0400

    setup.py: Update author email address

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='pynuodb',
     version=VERSION,
     author='NuoDB',
-    author_email='info@nuodb.com',
+    author_email='drivers@nuodb.com',
     description='NuoDB Python driver',
     keywords='nuodb scalable cloud database',
     packages=find_packages(),
