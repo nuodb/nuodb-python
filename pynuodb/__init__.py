7cd471b813f50e8b0228380e041397c78d81a03a
Eusebio Resende <eresende@nuodb.com>
2020-11-18 10:20:44 -0500

    Update to version v2.5.2

--- pynuodb/__init__.py
+++ pynuodb/__init__.py
@@ -6,7 +6,7 @@ This software is licensed under a BSD 3-Clause License.
 See the LICENSE file provided with this software.
 """
 
-__version__ = '2.5.1'
+__version__ = '2.5.2'
 
 from .connection import *
 from .datatype import *
