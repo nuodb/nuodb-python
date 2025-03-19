5ee780c555e73da42c5d32b2d9a56c7b1e513072
Paul Smith <paul@mad-scientist.net>
2025-03-19 18:05:18 -0400

    Update protocol version to DDL_NOT_AUTOCOMMITTED
    There are no client-side changes to the Python driver for this.

--- pynuodb/protocol.py
+++ pynuodb/protocol.py
@@ -369,5 +369,5 @@ PREPARE_AND_EXECUTE_TOGETHER                    = 26
 # The newest feature this driver supports.
 # The server will negotiate the highest compatible version.
 CURRENT_PROTOCOL_MAJOR     = 1
-CURRENT_PROTOCOL_VERSION   = SEND_PREPARE_STMT_RESULT_SET_METADATA_TO_CLIENT
+CURRENT_PROTOCOL_VERSION   = DDL_NOT_AUTOCOMMITTED
 AUTH_TEST_STR              = 'Success!'
