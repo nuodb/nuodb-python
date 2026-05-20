dacc8ddaa85c3fe4ea2ef39c5d582d84a2ecaf04
Paul Smith <paul@mad-scientist.net>
2026-05-20 11:52:58 -0400

    Update to version 4.0.1

--- pynuodb/__init__.py
+++ pynuodb/__init__.py
@@ -1,12 +1,12 @@
 """An implementation of Python PEP 249 for NuoDB.
 
-(C) Copyright 2013-2025 Dassault Systemes SE.  All Rights Reserved.
+(C) Copyright 2013-2026 Dassault Systemes SE.  All Rights Reserved.
 
 This software is licensed under a BSD 3-Clause License.
 See the LICENSE file provided with this software.
 """
 
-__version__ = '4.0.0'
+__version__ = '4.0.1'
 
 from .connection import *  # pylint: disable=wildcard-import
 from .datatype import *    # pylint: disable=wildcard-import
